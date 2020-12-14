111e6a234b3f266b50a9129efc0de2ffb573fd45
Mattias Bonte <wisecodr@gmail.com>
2020-12-14 14:40:36 +0100

    Add Book Adding Functionality

---
+++ 04_BOOKLIST/app.js
@@ -0,0 +1,52 @@
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+UI.prototype.addBookToList = (book) => {
+  const list = document.querySelector('#book-list');
+  // Create tr element
+  const row = document.createElement('tr');
+  // Insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete"X</a></td>
+  `;
+  list.appendChild(row);
+};
+
+// Clear Fields
+UI.prototype.clearFields = () => {
+  document.querySelector('#title').value = '';
+  document.querySelector('#author').value = '';
+  document.querySelector('#isbn').value = '';
+};
+
+// Listeners
+document.querySelector('#book-form').addEventListener('submit', (e) => {
+  // Get form values
+  const title = document.querySelector('#title').value,
+    author = document.querySelector('#author').value,
+    isbn = document.querySelector('#isbn').value;
+
+  // Instantiate Book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate UI
+  const ui = new UI();
+
+  // Add book to list
+  ui.addBookToList(book);
+
+  // Clear fields
+  ui.clearFields();
+
+  e.preventDefault();
+});
