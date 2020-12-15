21f53140a2259f699e69402f4d2d455f76474910
Mattias Bonte <wisecodr@gmail.com>
2020-12-15 16:09:23 +0100

    Finish github user search app

--- 06_GITHUBFINDER/github.js
+++ 06_GITHUBFINDER/github.js
@@ -1,7 +1,9 @@
 class Github {
   constructor() {
     this.client_id = '1500ae0046722fff44f5';
-    this.client_secret = '74bc5fcec908cbcf6dfcc0621429ce9e7adf3ae7';
+    this.client_secret = '07e234f94ea36f04ec876695d4bd0c7f7fe20aba';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
@@ -9,7 +11,12 @@ class Github {
       `https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`
     );
 
+    const repoResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`
+    );
+
     const profile = await profileResponse.json();
-    return { profile };
+    const repos = await repoResponse.json();
+    return { profile, repos };
   }
 }
